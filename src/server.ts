3f260c4f43fdf3f555430b10e9a426330f666f5e
Sairaj <srkhandagale1601@gmail.com>
2026-07-12 11:05:40 +0530

    chore: add pino logger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { config } from "@/config/env";
+import { logger } from "@/config/logger";
 import app from "./app";
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
   console.log(`Listening to Port ${PORT}`);
 });
+logger.info("Server Started ");
